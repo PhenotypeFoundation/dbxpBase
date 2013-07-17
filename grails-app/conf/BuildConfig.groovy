dd31928fed66422e0ea231cf75ce75f9e04c5fe9
Seth Snel <sethsnel@gmail.com>
2013-07-17 13:34:11 +0200

    Update version of gdt plugin and updated dbxpBase version

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -44,7 +44,7 @@ grails.project.dependency.resolution = {
         }
 
         compile(
-        ":gdt:0.4.2.2",
+        ":gdt:0.4.2.3",
 		":hibernate:$grailsVersion",
                 ":ajaxflow:latest.integration",
                 ":jquery:latest.integration",
