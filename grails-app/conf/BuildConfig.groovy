d31b2cecce8a4599a6fd84907cdb92ebfdbb45d7
Ferry Jagers <FerryJagers@gmail.com>
2014-04-07 16:05:21 +0200

    dbxpBase (-> GDT) version up

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -44,7 +44,7 @@ grails.project.dependency.resolution = {
         }
 
         compile(
-        ":gdt:0.5.0.1",
+        ":gdt:0.5.0.2",
         ":rest:0.8",
 		":hibernate:$grailsVersion",
                 ":ajaxflow:latest.integration",
