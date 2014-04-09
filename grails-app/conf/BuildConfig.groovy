720cb7212320190bac4643b8c0ad65dd5dbc4363
Ferry Jagers <FerryJagers@gmail.com>
2014-04-09 12:07:41 +0200

    GDT version up

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -44,7 +44,7 @@ grails.project.dependency.resolution = {
         }
 
         compile(
-        ":gdt:0.5.0.2.rc1",
+        ":gdt:0.5.0.2.rc2",
         ":rest:0.8",
 		":hibernate:$grailsVersion",
                 ":ajaxflow:latest.integration",
