5be3f51fcd6c7c8bce6f0a1e967bb547b58c3c71
Ferry Jagers <FerryJagers@gmail.com>
2014-04-24 13:27:00 +0200

    GDT version up

--- DbxpBaseGrailsPlugin.groovy
+++ DbxpBaseGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class DbxpBaseGrailsPlugin {
     // the plugin version
-    def version = "0.2.0.3.rc2"
+    def version = "0.2.0.3.rc3"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
     // the other plugins this plugin depends on
--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -44,7 +44,7 @@ grails.project.dependency.resolution = {
         }
 
         compile(
-        ":gdt:0.5.0.2.rc2",
+        ":gdt:0.5.0.2.rc3",
         ":rest:0.8",
 		":hibernate:$grailsVersion",
                 ":ajaxflow:latest.integration",
