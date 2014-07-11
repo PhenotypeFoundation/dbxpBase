d61a9319b20520780e450bcb8e3a8c0f05560978
Ferry Jagers <ferry.programming@iCloud.com>
2014-07-11 11:05:48 +0200

    Fixed deleting a subject with measurements

--- DbxpBaseGrailsPlugin.groovy
+++ DbxpBaseGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class DbxpBaseGrailsPlugin {
     // the plugin version
-    def version = "0.2.0.3.rc5"
+    def version = "0.2.0.4"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
     // the other plugins this plugin depends on
--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -44,7 +44,7 @@ grails.project.dependency.resolution = {
         }
 
         compile(
-        ":gdt:0.5.0.2.rc5",
+        ":gdt:0.5.0.3",
         ":rest:0.8",
 		":hibernate:$grailsVersion",
                 ":ajaxflow:latest.integration",
